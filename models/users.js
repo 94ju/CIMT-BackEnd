25b09ffe8fa80177431a0aca5dfcea837129d029
94ju <janithherath94@gmail.com>
2019-09-22 08:43:20 +0530

    bcrypt installed

--- models/users.js
+++ models/users.js
@@ -2,7 +2,9 @@ const mongoose= require('mongoose')
 
 const usersSchema=mongoose.Schema({
     email:{type:String,required:true},
-    password:{type:String,required:true}
+    password:{type:String,required:true},
+    username:{type:String},
+    role:{type:String}
 })
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const User =require("../models/users");
-
+const bcrypt= require('bcrypt')
 const router = express.Router();
-
-
-
 
 router.get('/',(req,res,next)=>{
     res.send('check')
+});
+
+router.post('/',(req,res,next)=>{
+    const user= new User({
+        email:req.body.email,
+        password:req.body.password,
+        username:req.body.username,
+        role:req.body.role
+    })
 })
+
+
 
 module.exports = router;
