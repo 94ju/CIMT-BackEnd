1b81f9c91d5e2b96041810587259312fef281402
94ju <janithherath94@gmail.com>
2019-09-22 12:07:37 +0530

    auth

--- routes/user.js
+++ routes/user.js
@@ -8,12 +8,30 @@ router.get('/',(req,res,next)=>{
 });
 
 router.post('/',(req,res,next)=>{
-    const user= new User({
-        email:req.body.email,
-        password:req.body.password,
-        username:req.body.username,
-        role:req.body.role
-    })
+    bcrypt.hash(req.body.password,10).
+        then(hash=>{
+            const user= new User({
+                email:req.body.email,
+                password:hash,
+                username:req.body.username,
+                role:req.body.role
+            });
+            user.save().
+                then(result=>{
+                    res.status(201).json({
+                        message:'user created',
+                        result:result
+                    })
+                }).
+                catch(err=>{
+                    res.status(500).json(
+                        {
+                            error:err
+                        }
+                    )
+                })
+        })
+   
 })
 
 
