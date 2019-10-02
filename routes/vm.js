012cf072a62526225c4811893ff68aa4ae31a7ea
94ju <janithherath94@gmail.com>
2019-10-03 01:04:52 +0530

    aws launch

--- routes/vm.js
+++ routes/vm.js
@@ -2,12 +2,18 @@ const express = require("express");
 const checkAuth =require('../middleware/check-auth');
 const Vm = require('../models/vm');
 const router = express.Router();
+var AWS = require('aws-sdk');
+
 
 router.get('/vmdata',checkAuth,(req,res,next)=>{
     res.send('check')
 });
-
 router.post('/createvm',checkAuth,(req,res)=>{
+    AWS.config.update({
+        accessKeyId:'AKIAQIT3JR7QEQSSDB5Y',
+        secretAccessKey:'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',
+        region:'us-east-1'
+    })
     const vm = new Vm({
         ami:req.body.ami,
         instanceType:req.body.instanceType,
@@ -29,10 +35,56 @@ router.post('/createvm',checkAuth,(req,res)=>{
             }
         )
     })
+    var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
+    var instanceParams = {
+        ImageId: vm.ami, 
+        InstanceType: vm.instanceType,
+        KeyName: 'checkapijanith',
+        MinCount: 1,
+        MaxCount: vm.numberOfInstances
+     };
+
+     var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
+     instancePromise.then(
+        function(data) {
+          console.log(data);
+          var instanceId = data.Instances[0].InstanceId;
+          console.log("Created instance", instanceId);
+          // Add tags to the instance
+          tagParams = {Resources: [instanceId], Tags: [
+             {
+                Key: 'Name',
+                Value: 'SDK Sample'
+             }
+          ]};
+          var tagPromise = new AWS.EC2({apiVersion: '2016-11-15'}).createTags(tagParams).promise();
+          tagPromise.then(
+            function(data) {
+              console.log("Instance tagged");
+            }).catch(
+              function(err) {
+              console.error(err, err.stack);
+            });
+        }).catch(
+          function(err) {
+          console.error(err, err.stack);
+        });
 })
 
+router.post('/vmstatus',(req,res)=>{
 
+var ec2 = new AWS.EC2({apiVersion: '2016-11-15'});
 
+var params = {
+  DryRun: false
+};
 
-
+ec2.describeInstances(params, function(err, data) {
+  if (err) {
+    console.log("Error", err.stack);
+  } else {
+    console.log("Success", JSON.stringify(data));
+  }
+});
+})
 module.exports = router;
